9d6a13549c34b07e222d363370747a9a8797d92f
letorvar <letorvar@gmail.com>
2017-06-07 20:55:24 +0200

    Implementing snake move (In progress)

--- sketch.js
+++ sketch.js
@@ -6,14 +6,51 @@ var meat = new Food();
 
 function setup() {
     createCanvas(canvasSize, canvasSize);
-
+    boaFrank.body.push(createVector(10, 10));
 }
 
 function Boa() {
-    this.x = boardSize / 2;
-    this.y = boardSize / 2;
+    //this.x = boardSize / 2;
+    //this.y = boardSize / 2;
+    this.body = [];
+
+    // this.body.push({ x: (boardSize / 2) - 1, y: boardSize / 2 });
+    // this.body.push({ x: (boardSize / 2) - 2, y: boardSize / 2 });
+    // this.body.push({ x: (boardSize / 2) - 3, y: boardSize / 2 });
+    console.log(this.body);
     this.direction = 'RIGHT';
     this.color = "#E9C46A";
+    this.move = function() {
+        var head = this.body[length - 1];
+        for (var i = 0; i < this.body.length - 1; i++) {
+            this.body[i] = this.body[i + 1];
+        }
+        this.body[this.body.length - 1] = createVector(head.x + 1, head.y);
+        // var head = this.body[this.body.length - 1];
+        // var tail = this.body.pop();
+        //     var i = this.body.length - 1;
+        //     while (i >= 0) {
+        //         console.log('Iteracja : ' + i);
+        //         if (i === 0) {
+        //             if (this.direction === 'RIGHT') {
+        //                 this.body[i].x = (this.body[i].x + 1) % boardSize;
+        //             } else if (this.direction === 'LEFT') {
+        //                 this.body[i].x = (this.body[i].x + boardSize - 1) % boardSize;
+        //             } else if (this.direction === 'UP') {
+        //                 this.body[i].y = (this.body[i].y + boardSize - 1) % boardSize;
+        //             } else if (this.direction === 'DOWN') {
+        //                 this.body[i].y = (this.body[i].y + boardSize + 1) % boardSize;
+        //             }
+        //         } else {
+        //             // var current = this.body[i];
+        //             var next = this.body[i - 1];
+        //             console.log('Przesuwam : ' + this.body[i].x + '-' + this.body[i].y + ' na ' + next.x + '-' + next.y);
+        //             this.body[i] = next;
+        //         }
+        //         i--;
+        //     }
+        //     // this.body.unshift(tail);
+    }
 }
 
 function Food() {
@@ -37,24 +74,21 @@ function keyPressed() {
 }
 
 function draw() {
-    frameRate(2);
+    frameRate(0.2);
     for (var x = 0; x < boardSize; x++) {
         for (var y = 0; y < boardSize; y++) {
             fill("#2A9D8F");
             rect(x * tileSize, y * tileSize, tileSize, tileSize);
         }
     }
-    fill(boaFrank.color);
-    rect(boaFrank.x * tileSize, boaFrank.y * tileSize, tileSize, tileSize);
-    if (boaFrank.direction === 'RIGHT') {
-        boaFrank.x = (boaFrank.x + 1) % boardSize;
-    } else if (boaFrank.direction === 'LEFT') {
-        boaFrank.x = (boaFrank.x + boardSize - 1) % boardSize;
-    } else if (boaFrank.direction === 'UP') {
-        boaFrank.y = (boaFrank.y + boardSize - 1) % boardSize;
-    } else if (boaFrank.direction === 'DOWN') {
-        boaFrank.y = (boaFrank.y + boardSize + 1) % boardSize;
+
+    for (var i = 0; i < boaFrank.body.length; i++) {
+        fill(boaFrank.color);
+        //console.log(i + ' : ' + boaFrank.body[i].x + ' ' + boaFrank.body[i].y);
+        rect((boaFrank.body[i].x) * tileSize, (boaFrank.body[i].y) * tileSize, tileSize, tileSize);
     }
+
+    boaFrank.move();
 
     fill(meat.color);
     rect(meat.x * tileSize, meat.y * tileSize, tileSize, tileSize);
