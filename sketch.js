445a1864ab12c8d81049ec6d8b7e41ecf481ecb3
letorvar <letorvar@gmail.com>
2017-06-15 22:28:16 +0200

    implemented self biting and reset game

--- sketch.js
+++ sketch.js
@@ -4,23 +4,21 @@ var tileSize = canvasSize / boardSize;
 var boaFrank = new Boa();
 var meat = new Food();
 var bg;
+var hasMoved = false;
 
 function setup() {
     createCanvas(canvasSize, canvasSize);
-    boaFrank.body.push(createVector(boardSize / 2, boardSize / 2));
-    boaFrank.body.push(createVector(boardSize / 2 - 1, boardSize / 2));
-    boaFrank.body.push(createVector(boardSize / 2 - 2, boardSize / 2));
-    boaFrank.body.push(createVector(boardSize / 2 - 3, boardSize / 2));
-    boaFrank.body.push(createVector(boardSize / 2 - 4, boardSize / 2));
-    boaFrank.body.push(createVector(boardSize / 2 - 5, boardSize / 2));
+    boaFrank.newGame();
 
-    bg = loadImage('img/background.jpg');
+
+    // bg = loadImage('img/background.jpg');
 }
 
 function Boa() {
     this.body = [];
     this.direction = 'RIGHT';
     this.color = "#E9C46A";
+
     this.eat = function() {
         var head = this.body[this.body.length - 1];
         var foodDistance = dist(head.x, head.y, meat.x, meat.y);
@@ -29,13 +27,31 @@ function Boa() {
             meat = new Food();
         }
     }
+    this.getHead = function(){
+        return this.body[this.body.length - 1]; 
+    }
     this.move = function() {
-        var head = this.body[this.body.length - 1];
+        var head = this.getHead(); 
         for (var i = 0; i < this.body.length - 1; i++) {
             this.body[i] = this.body[i + 1];
         }
         this.body[this.body.length - 1] = this.nextStep(head.x, head.y, this.direction, boardSize);
+        if (this.checkIfBite()){
+            this.newGame();
+        }
+        hasMoved = false;
     }
+    this.checkIfBite = function(){
+        var head = this.getHead(); 
+        for(var i = 0; i < this.body.length - 1; i++){
+            var snakeElement = this.body[i];
+            if (snakeElement.x === head.x && snakeElement.y === head.y){
+                return true;
+            }
+        }
+        return false; 
+    }
+
     this.nextStep = function(x,y, direction, boardSize){
         if (direction === 'RIGHT') {
             return createVector((x + 1) % boardSize,y);
@@ -47,6 +63,10 @@ function Boa() {
             return createVector(x, (y + 1) % boardSize);
         }
     }
+    this.newGame = function(){
+        this.body = [];
+        this.body.push(createVector(boardSize / 2, boardSize / 2));
+    }
 }
 
 function Food() {
@@ -54,29 +74,30 @@ function Food() {
     this.y = Math.floor(Math.random() * boardSize);
     this.color = "#E76F51";
 }
-
 function keyPressed() {
-    if (keyCode === LEFT_ARROW && boaFrank.direction !== 'RIGHT') {
-        boaFrank.direction = 'LEFT';
-    } else if (keyCode === RIGHT_ARROW && boaFrank.direction !== 'LEFT') {
-        boaFrank.direction = 'RIGHT';
-    } else if (keyCode === DOWN_ARROW && boaFrank.direction !== 'UP') {
-        boaFrank.direction = 'DOWN';
-    } else if (keyCode === UP_ARROW && boaFrank.direction !== 'DOWN') {
-        boaFrank.direction = 'UP';
-    }
+    if (!hasMoved){
+        if (keyCode === LEFT_ARROW && boaFrank.direction !== 'RIGHT') {
+            boaFrank.direction = 'LEFT';
+        } else if (keyCode === RIGHT_ARROW && boaFrank.direction !== 'LEFT') {
+            boaFrank.direction = 'RIGHT';
+        } else if (keyCode === DOWN_ARROW && boaFrank.direction !== 'UP') {
+            boaFrank.direction = 'DOWN';
+        } else if (keyCode === UP_ARROW && boaFrank.direction !== 'DOWN') {
+            boaFrank.direction = 'UP';
+        }
+    } 
+    hasMoved = true;
 }
-
 function draw() {
-    frameRate(5);
+    frameRate(5 + boaFrank.body.length);
     noStroke();
-    background(bg);
-    // for (var x = 0; x < boardSize; x++) {
-    //     for (var y = 0; y < boardSize; y++) {
-    //         fill(42,157,142, 100);
-    //         rect(x * tileSize, y * tileSize, tileSize, tileSize);
-    //     }
-    // }
+    // background(bg);
+    for (var x = 0; x < boardSize; x++) {
+        for (var y = 0; y < boardSize; y++) {
+            fill(42,157,142, 100);
+            rect(x * tileSize, y * tileSize, tileSize, tileSize);
+        }
+    }
 
     for (var i = 0; i < boaFrank.body.length; i++) {
         fill(boaFrank.color);
